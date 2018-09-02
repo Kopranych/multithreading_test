c98bc1be30dbd9c0a89f16fd21b0081d0e83caf0
Kopranych <kopranych@gmail.com>
2018-09-02 22:24:40 +0300

    [CREATE] - multithreading_test создал проект

---
+++ src/main/java/threads/ThreadReader.java
@@ -0,0 +1,9 @@
+package threads;
+
+public class ThreadReader extends Thread {
+
+    @Override
+    public void run(){
+
+    }
+}
---
+++ src/main/java/threads/ThreadWriter.java
@@ -0,0 +1,16 @@
+package threads;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class ThreadWriter extends Thread {
+    @Override
+    public void run(){
+        Scanner scanner = new Scanner(System.in);
+
+       String in = scanner.nextLine();
+
+    }
+}
